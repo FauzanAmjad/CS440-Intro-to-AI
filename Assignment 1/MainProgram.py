7dd6ff0a2761e35d20ec7a6b80846c0b6c9ad396
MJ <mjani@yahoo.com>
2022-02-03 03:20:03 -0500

    started on algo imp

---
+++ Assignment 1/MainProgram.py
@@ -0,0 +1,50 @@
+
+
+firstnode=None
+
+
+
+
+def setdata():
+   count=0
+   
+
+
+
+
+
+def AStar():
+
+
+
+
+
+
+
+
+def Thetastar():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    setdata()
+    Astar()
+    Thetastar()
+    print("Done.")
