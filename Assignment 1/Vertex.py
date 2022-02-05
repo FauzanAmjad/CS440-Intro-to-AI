8c629cd311b0696d4cb6502b1af6bd80fb1dfbf7
YousefAttia-git <yattia22@gmail.com>
2022-02-05 02:51:47 -0500

    fixed up display function and Vertex class, working on tooltips

--- Assignment 1/Vertex.py
+++ Assignment 1/Vertex.py
@@ -2,37 +2,28 @@ import pygame
 
 
 class vertex:
-    neighbors = []
-    parent = None
-    is_closed = False
-    gvalue = 0
-    hvalue = 0
 
     def __init__(self, img_coords: tuple, coords: tuple):
         self.img_coords = img_coords
         self.coords = coords
-
-    def get_img_coords(self):
-        return self.img_coords
-
-    def get_coords(self):
-        return self.coords
+        self.neighbors = []
+        self.parent = None
+        self.is_closed = False
+        self.gvalue = 0
+        self.hvalue = 0
+        self.rect = None
 
     def draw_vertex(self, surface):
         pygame.draw.circle(surface, (0, 0, 0), self.img_coords, 5)
+        self.rect = pygame.Rect(0, 0, 10, 10)
+        self.rect.center = self.img_coords
 
     def draw_lines(self, surface):
         for vert in self.neighbors:
-            pygame.draw.line(surface, (0, 0, 0), self.img_coords, vert.get_img_coords())
+            pygame.draw.line(surface, (0, 0, 0), self.img_coords, vert.img_coords)
 
     def __str__(self):
         return f"img coords: {self.img_coords} coords: {self.coords}"
-    
+
     def close(self):
-        self.is_closed= True
-    def set_parent(self, newparent):
-        self.parent = newparent
-    def set_g(self, newg):
-        self.gvalue=newg
-    def set_h(self, newh):
-        self.hvalue=newh
+        self.is_closed = True
--- Assignment 1/display.py
+++ Assignment 1/display.py
@@ -1,4 +1,4 @@
-import pygame, sys, os
+import pygame, sys, os, pygame_gui
 from Vertex import vertex
 
 data = [
@@ -8,89 +8,91 @@ data = [
 ]
 
 vertices = {}
-filewidth=0
-filelength=0
-blockedcells=[]
-filelist=[]
-startx=0
-starty=0
-goalx=0
-goaly=0
-blockedxcoord=[]
-blockedycoord=[]
-
+filewidth = 0
+filelength = 0
+blockedcells = []
+filelist = []
+startx = 0
+starty = 0
+goalx = 0
+goaly = 0
+blockedxcoord = []
+blockedycoord = []
 
 
 def main():
     pygame.init()
-    window = pygame.display.set_mode((960, 720))
+    window = pygame.display.set_mode((960, 600))
     window.fill((255, 255, 255))
-
+    clicked = False
     view_rect = pygame.Rect(0, 0, 480, 360)
     view_rect.center = window.get_rect().center
     pygame.display.set_caption('A*/Theta* simulation')
     draw_grid(window, view_rect.width, view_rect.height, 4, view_rect)
+    for n in vertices[(0, 0)].neighbors:
+        print(n.coords)
     while True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-                sys.exit()
+        if pygame.event.get(pygame.QUIT):
+            pygame.display.quit()
+            pygame.quit()
+            sys.exit()
+        if pygame.event.get(pygame.MOUSEBUTTONDOWN):
+            for key in vertices:
+                if vertices[key].rect.collidepoint(pygame.mouse.get_pos()):
+                    clicked = not clicked
+                    print(f"clicked vertex:{key}")
+                    if clicked:
+                        draw_tooltip(window, 60, 120, vertices[key].img_coords[0], vertices[key].img_coords[1])
+
         pygame.display.update()
 
+
 def readfolder(foldername):
-    filelist=os.list("testfiles/")
+    filelist = os.list("testfiles/")
 
 
 def readfile(filename):
-    f=open(filename,"r")
-    count=1
+    f = open(filename, "r")
+    count = 1
     for line in f:
-        internalcount=1
-        localx=0
-        localy=0
+        internalcount = 1
+        localx = 0
+        localy = 0
         for word in line.split():
 
             if count == 1:
                 if internalcount == 1:
-                    startx=int(word)
+                    startx = int(word)
                 elif internalcount == 2:
-                    starty=int(word)
-            elif count ==2:
+                    starty = int(word)
+            elif count == 2:
                 if internalcount == 1:
-                    goalx=int(word)
+                    goalx = int(word)
                 elif internalcount == 2:
-                    goaly=int(word)
-            elif count ==3:
+                    goaly = int(word)
+            elif count == 3:
                 if internalcount == 1:
-                    filewidth=int(word)
+                    filewidth = int(word)
                 elif internalcount == 2:
-                    filelength=int(word)
+                    filelength = int(word)
             else:
 
                 if internalcount == 1:
-                    localx=int(word)
+                    localx = int(word)
                 elif internalcount == 2:
-                    localy=int(word)
+                    localy = int(word)
                 elif internalcount == 3:
-                    indicator=int(word)
-                    if indicator==1:
+                    indicator = int(word)
+                    if indicator == 1:
                         blockedxcoord.append(localx)
                         blockedycoord.append(localy)
-            internalcount=internalcount+1
-        count=count+1
-    setupnodes()
-
-def setupnodes():
-    
+            internalcount = internalcount + 1
+        count = count + 1
+    # setupnodes()
 
 
-
-
-
-
-
+# def setupnodes():
 
 
 def draw_grid(window, width, height, cols, view):
@@ -103,14 +105,16 @@ def draw_grid(window, width, height, cols, view):
             if data[i][j] == 1:
                 pygame.draw.rect(window, (174, 174, 174), rect)
             else:
-                pygame.draw.rect(window, (0, 0, 0), rect, 1)
-                add_verts((x, y), (i, j), size)
+                #pygame.draw.rect(window, (0, 0, 0), rect, 1)
+                add_verts((x, y), (j, i), size)
             i = i + 1
         j = j + 1
         i = 0
 
     for key in vertices:
         vertices[key].draw_vertex(window)
+        vertices[key].draw_lines(window)
+        #print(f"{vertices[key].get_coords()}->{vertices[key].get_img_coords()}")
 
 
 def create_vert(img_coords: tuple, coords: tuple):
@@ -126,8 +130,13 @@ def add_verts(img_coords: tuple, coords: tuple, size):
              create_vert((img_coords[0], img_coords[1] + size), (coords[0], coords[1] + 1)),
              create_vert((img_coords[0] + size, img_coords[1] + size), (coords[0] + 1, coords[1] + 1))]
     for i in range(0, len(verts)):
-        for j in range(i, len(verts)):
-            verts[i].neighbors.append(verts[j])
+        for j in range(0, len(verts)):
+            if j != i:
+                verts[i].neighbors.append(verts[j])
+                print(f"{verts[i].coords}->{verts[j].coords}")
 
+def draw_tooltip(surface, width, height, x,y):
+    rect = pygame.Rect(x, y, width, height)
+    pygame.draw.rect(surface, (208, 208, 208), rect, border_radius= width//8)
 
 main()
