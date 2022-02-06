c1231efc03a1eae22d8b31ac1f55b0afc1db7a7e
MJ <mjani@yahoo.com>
2022-02-06 01:59:38 -0500

    begin of algo

--- Assignment 1/display.py
+++ Assignment 1/display.py
@@ -1,5 +1,7 @@
 import pygame, sys, os, pygame_gui
-from Vertex import vertex
+from .Vertex import vertex
+import heapq
+import math
 
 data = [
     [0, 0, 0, 0],
@@ -14,13 +16,31 @@ blockedcells = []
 filelist = []
 startx = 0
 starty = 0
+startindex =0
+endindex=0
 goalx = 0
 goaly = 0
 blockedxcoord = []
 blockedycoord = []
+temparr= None
+directory=""
 
 
 def main():
+
+    #specify input type
+    print("This program either takes input in File Form or in Auto-Generated Form")
+    inputform=input("Enter \"File\" or \"Auto-Generated\" for Input Type:")
+
+    if inputform=="File":
+       file=input("Please enter path where test files is located: ")
+       readfile(file)
+    elif inputform=="Auto-Generated":
+        print()
+       #logic for this
+    else:
+        print("Invalid Input Type; Program is Exiting")
+        exit()
     # making the display
     grid_width = 480
     grid_height = 360
@@ -47,8 +67,8 @@ def main():
         manager.draw_ui(window)
 
 
-def readfolder(foldername):
-    filelist = os.list("testfiles/")
+
+
 
 
 def readfile(filename):
@@ -76,7 +96,8 @@ def readfile(filename):
                 elif internalcount == 2:
                     filelength = int(word)
             else:
-
+                if count == 4:
+                    temparr = [[0] * filewidth] * filelength
                 if internalcount == 1:
                     localx = int(word)
                 elif internalcount == 2:
@@ -86,6 +107,9 @@ def readfile(filename):
                     if indicator == 1:
                         blockedxcoord.append(localx)
                         blockedycoord.append(localy)
+                        temparr[localy-1][localx-1]=1
+
+
             internalcount = internalcount + 1
         count = count + 1
     # setupnodes()
@@ -111,6 +135,11 @@ def draw_grid(window, width, height, cols, view, manager):
         i = 0
 
     for key in vertices:
+        if vertices[key].coords[0]==startx and vertices[key].coords[1]==starty:
+            startindex=key
+        if vertices[key].coords[0]==goalx and vertices[key].coords[1]==goaly:
+            endindex=key
+
         vertices[key].draw_vertex(window, manager)
         vertices[key].draw_lines(window)
         # print(f"{vertices[key].get_coords()}->{vertices[key].get_img_coords()}")
@@ -137,6 +166,93 @@ def add_verts(img_coords: tuple, coords: tuple, size):
 def draw_tooltip(surface, width, height, x, y):
     rect = pygame.Rect(x, y, width, height)
     pygame.draw.rect(surface, (208, 208, 208), rect, border_radius=width // 8)
+
+def hfunction(pointx, pointy):
+    root2=math.sqrt(2)
+    xminusgoal=abs(pointx-goalx)
+    yminusgoal=abs(pointy-goaly)
+    answer=(root2*min(xminusgoal,yminusgoal))+max(xminusgoal,yminusgoal)-min(xminusgoal,yminusgoal)
+    return answer
+def lineofsight(sourcex, sourcey,pointx, pointy):
+    x0=sourcex
+    y0=sourcey
+    x1=pointx
+    y1=pointy
+    f=0
+    dy=y1-y0
+    dx=x1-x0
+    sy=0
+    sx=0
+    if dy<0:
+        dy= -1*dy
+        sy=-1
+    else:
+        sy=1
+    if dx<0:
+        dx= -1*dx
+        sx= -1
+    else:
+        sx=1
+    if dx >= dy:
+        while (x0 != x1):
+            f=f+dy
+            addx0 = -1
+            addy0 = -1
+            if sx == 1:
+                addx0 = 0
+            if sy == 1:
+                addy0 = 0
+            blocked= data[x0+addx0-1][y0+addy0-1]
+            blocked2=data[x0+addx0-1][y0-1]
+            blocked3=data[x0+addx0-1][y0-1-1]
+            if f >= dx:
+                if blocked == 1:
+                    return False
+                y0=y0+sy
+                f=f-dx
+
+            if f!=0 and blocked==1:
+                return False
+            if dy==0 and blocked2==1 and blocked3==1:
+                return False
+            x0=x0+sx
+
+
+    else:
+        while (y0 != y1):
+            f=f+dx
+            addx0 = -1
+            addy0 = -1
+            if sx == 1:
+                addx0 = 0
+            if sy == 1:
+                addy0 = 0
+            blocked= data[x0+addx0-1][y0+addy0-1]
+            blocked2=data[x0-1][y0+addy0-1]
+            blocked3=data[x0-1-1][y0+addy0-1]
+            if f >= dy:
+                if blocked == 1:
+                    return False
+                x0=x0+sx
+                f=f-dy
+
+            if f!=0 and blocked==1:
+                return False
+            if dx==0 and blocked2==1 and blocked3==1:
+                return False
+            y0=y0+sy
+    return True
+
+
+def Astar():
+    #Compute h for all of them
+
+    #Start
+
+    #Update Vertex
+
+
+def Thetastar():
 
 
 main()
